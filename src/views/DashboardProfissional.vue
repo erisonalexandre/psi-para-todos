4ccf1ed0b324761ccb9b5cb71147d68c23b8299d
erison <erison@flexpeak.com.br>
2020-04-18 17:47:35 -0400

    adicionando header no card

--- src/views/DashboardProfissional.vue
+++ src/views/DashboardProfissional.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login">
+  <div class="altura-100-centro">
     <div class="row">
       <div class="col-md-4">
 
@@ -19,42 +19,43 @@
           <h1>Seja um Herói Ouro!</h1>
         </card>
         <br>
-        <table class="table table-borderless">
-          <tr>
-            <th class="tg-baqh" colspan="4">Agendamentos pendentes de confirmação</th>
-          </tr>
-
-          <tr>
-            <td class="tg-hmp3">Paciente</td>
-            <td class="tg-hmp3">Horário</td>
-            <td class="tg-hmp3">Data</td>
-            <td class="tg-hmp3">Ação</td>
-          </tr>
-          <tr>
-            <td class="tg-0lax">Sr. Amarildo</td>
-            <td class="tg-0lax">17:00h PM</td>
-            <td class="tg-lqy6">18/04/2020<br></td>
-            <td class="tg-0lax">img img</td>
-          </tr>
-          <tr>
-            <td class="tg-hmp3">Sra. Bruna</td>
-            <td class="tg-hmp3"><span style="font-weight:400;font-style:normal">11:00h AM</span><br></td>
-            <td class="tg-mb3i"><span style="font-weight:400;font-style:normal">19/04/2020</span></td>
-            <td class="tg-hmp3">img img</td>
-          </tr>
-          <tr>
-            <td class="tg-0lax">Sra Renata</td>
-            <td class="tg-0lax">14:00 PM</td>
-            <td class="tg-lqy6"><span style="font-weight:400;font-style:normal">21/04/2020</span><br></td>
-            <td class="tg-0lax">img img</td>
-          </tr>
-          <tr>
-            <td class="tg-hmp3">Sr Apollo</td>
-            <td class="tg-hmp3">16:00 PM</td>
-            <td class="tg-hmp3"><span style="font-weight:400;font-style:normal">20/04/2020</span></td>
-            <td class="tg-hmp3">img img</td>
-          </tr>
+        <card>
+          <template slot="header">
+            Agendamentos pendentes de confirmação
+          </template>
+          <table class="table table-borderless">
+            <tr>
+              <td class="tg-hmp3">Paciente</td>
+              <td class="tg-hmp3">Horário</td>
+              <td class="tg-hmp3">Data</td>
+              <td class="tg-hmp3">Ação</td>
+            </tr>
+            <tr>
+              <td class="tg-0lax">Sr. Amarildo</td>
+              <td class="tg-0lax">17:00h PM</td>
+              <td class="tg-lqy6">18/04/2020<br></td>
+              <td class="tg-0lax">img img</td>
+            </tr>
+            <tr>
+              <td class="tg-hmp3">Sra. Bruna</td>
+              <td class="tg-hmp3"><span style="font-weight:400;font-style:normal">11:00h AM</span><br></td>
+              <td class="tg-mb3i"><span style="font-weight:400;font-style:normal">19/04/2020</span></td>
+              <td class="tg-hmp3">img img</td>
+            </tr>
+            <tr>
+              <td class="tg-0lax">Sra Renata</td>
+              <td class="tg-0lax">14:00 PM</td>
+              <td class="tg-lqy6"><span style="font-weight:400;font-style:normal">21/04/2020</span><br></td>
+              <td class="tg-0lax">img img</td>
+            </tr>
+            <tr>
+              <td class="tg-hmp3">Sr Apollo</td>
+              <td class="tg-hmp3">16:00 PM</td>
+              <td class="tg-hmp3"><span style="font-weight:400;font-style:normal">20/04/2020</span></td>
+              <td class="tg-hmp3">img img</td>
+            </tr>
           </table>
+        </card>
       </div>
     </div>
   </div>
