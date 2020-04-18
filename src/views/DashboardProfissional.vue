688fb2b7eac7ac785d41237b1d55fa2e84b20703
fcsouza <fcsouza18@hotmail.com>
2020-04-18 15:10:45 -0300

    estrutura basica

--- src/views/DashboardProfissional.vue
+++ src/views/DashboardProfissional.vue
@@ -1,11 +1,61 @@
 <template>
   <div class="login">
-    <card class="px-4">
-        <h1 class="text-center text-psi" style="margin-bottom: 4rem">Seus Atendimentos</h1>
-    </card>
-    <card class="px-4">
-        <h1 class="text-center text-psi" style="margin-bottom: 4rem">Atendimentos</h1>
-    </card>
+    <div class="row">
+      <div class="col-md-4">
+        <card class="px-4">
+          <h1 class="text-center text-psi" style="margin-bottom: 4rem">Seus Atendimentos</h1>
+        </card>
+        <card class="px-4">
+            <h1 class="text-center text-psi" style="margin-bottom: 4rem">Atendimentos</h1>
+        </card>
+      </div>
+      <div class="col-md-8">
+        <card class="d-flex justify-content-start">
+            <p>Você atendeu</p>
+            <h1>3 vezes esse mês</h1>
+        </card>
+        <card class="px-4">
+            <h1 class="text-center text-psi" style="margin-bottom: 4rem">Seja um Herói Ouro!</h1>
+        </card>
+        <table class=".table-borderless">
+
+        <tr>
+          <th class="tg-baqh" colspan="4">Agendamentos pendentes de confirmação</th>
+        </tr>
+
+        <tr>
+          <td class="tg-hmp3">Paciente</td>
+          <td class="tg-hmp3">Horário</td>
+          <td class="tg-hmp3">Data</td>
+          <td class="tg-hmp3">Ação</td>
+        </tr>
+        <tr>
+          <td class="tg-0lax">Sr. Amarildo</td>
+          <td class="tg-0lax">17:00h PM</td>
+          <td class="tg-lqy6">18/04/2020<br></td>
+          <td class="tg-0lax">img img</td>
+        </tr>
+        <tr>
+          <td class="tg-hmp3">Sra. Bruna</td>
+          <td class="tg-hmp3"><span style="font-weight:400;font-style:normal">11:00h AM</span><br></td>
+          <td class="tg-mb3i"><span style="font-weight:400;font-style:normal">19/04/2020</span></td>
+          <td class="tg-hmp3">img img</td>
+        </tr>
+        <tr>
+          <td class="tg-0lax">Sra Renata</td>
+          <td class="tg-0lax">14:00 PM</td>
+          <td class="tg-lqy6"><span style="font-weight:400;font-style:normal">21/04/2020</span><br></td>
+          <td class="tg-0lax">img img</td>
+        </tr>
+        <tr>
+          <td class="tg-hmp3">Sr Apollo</td>
+          <td class="tg-hmp3">16:00 PM</td>
+          <td class="tg-hmp3"><span style="font-weight:400;font-style:normal">20/04/2020</span></td>
+          <td class="tg-hmp3">img img</td>
+        </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
