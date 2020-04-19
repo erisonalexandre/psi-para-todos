8527122b77eeb06f7e3cbb652273a8bc7c126534
erison <erison@flexpeak.com.br>
2020-04-19 13:48:05 -0400

    corrigindo layout dashboard profissional

--- src/views/DashboardProfissional.vue
+++ src/views/DashboardProfissional.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="altura-100-centro">
+  <div class="altura-100-centro container">
     <div class="row">
       <div class="col-md-4">
+        <div class="d-flex justify-content-center">
           <img :src="require('../assets/Agendamentos.svg')" class="img-fluid" width="350px">
+        </div>
+        <div class="d-flex justify-content-center">
           <img :src="require('../assets/agendamento.svg')" class="img-fluid" width="350px">
+        </div>
       </div>
       <div class="col-md-8">
-        <div>
-          <card class="card card1">
-              <p>Você atendeu</p>
-              <h1>3 vezes esse mês</h1>
+        <div class="row mx-0 justify-content-between">
+          <card class="card1 col-lg-5 col-12 mb-3 mb-lg-0">
+            <p>Você atendeu</p>
+            <h1>3 vezes esse mês</h1>
           </card>
-          <card class="card1">
-            <h1>Seja um Herói Ouro!</h1>
+          <card class="card1 col-lg-5 col-12">
+            <div class="d-flex align-items-center h-100">
+              <h1>Seja um Herói Ouro!</h1>
+            </div>
           </card>
         </div>
         <br>
@@ -34,8 +40,8 @@
               <td class="tg-hmp38">17:00h PM</td>
               <td class="tg-hmp38">18/04/2020<br></td>
               <td class="tg-hmp38">
-                <input type="image" id="image" width="15px" alt="confirm" :src="require('../assets/confirm.svg')">
-                <input type="image" id="image" width="15px" alt="confirm" :src="require('../assets/cancel.svg')">
+                <input type="image" width="15px" alt="confirm" :src="require('../assets/confirm.svg')">
+                <input type="image" width="15px" alt="confirm" :src="require('../assets/cancel.svg')">
               </td>
             </tr>
             <tr>
@@ -43,8 +49,8 @@
               <td class="tg-hmp38"><span style="font-weight:400;font-style:normal">11:00h AM</span><br></td>
               <td class="tg-hmp38"><span style="font-weight:400;font-style:normal">19/04/2020</span></td>
               <td class="tg-hmp38">
-                <input type="image" id="image" width="15px" alt="confirm" :src="require('../assets/confirm.svg')">
-                <input type="image" id="image" width="15px" alt="confirm" :src="require('../assets/cancel.svg')">
+                <input type="image" width="15px" alt="confirm" :src="require('../assets/confirm.svg')">
+                <input type="image" width="15px" alt="confirm" :src="require('../assets/cancel.svg')">
               </td>
             </tr>
             <tr>
@@ -52,8 +58,8 @@
               <td class="tg-hmp38">14:00 PM</td>
               <td class="tg-hmp38"><span style="font-weight:400;font-style:normal">21/04/2020</span><br></td>
               <td class="tg-hmp38">
-                <input type="image" id="image" width="15px" alt="confirm" :src="require('../assets/confirm.svg')">
-                <input type="image" id="image" width="15px" alt="confirm" :src="require('../assets/cancel.svg')">
+                <input type="image" width="15px" alt="confirm" :src="require('../assets/confirm.svg')">
+                <input type="image" width="15px" alt="confirm" :src="require('../assets/cancel.svg')">
               </td>
             </tr>
             <tr>
@@ -61,8 +67,8 @@
               <td class="tg-hmp38">16:00 PM</td>
               <td class="tg-hmp38"><span style="font-weight:400;font-style:normal">20/04/2020</span></td>
               <td class="tg-hmp38">
-                <input type="image" id="image" width="15px" alt="confirm" :src="require('../assets/confirm.svg')">
-                <input type="image" id="image" width="15px" alt="confirm" :src="require('../assets/cancel.svg')">
+                <input type="image" width="15px" alt="confirm" :src="require('../assets/confirm.svg')">
+                <input type="image" width="15px" alt="confirm" :src="require('../assets/cancel.svg')">
               </td>
             </tr>
           </table>
@@ -79,34 +85,13 @@ export default {
 </script>
 
 <style lang="scss">
-  .login {
-    display: flex;
-    justify-content: left;
-    align-items: left;
-    border-radius: 20%;
-    overflow-y: auto;
-    overflow-x: hidden;
-    height: calc(100% - 81px);
-  }
-  .login > .card {
-    width: 100%;
-    padding: 15px;
-    margin: auto;
-    max-width: 450px;
-    h1{
-      font-weight: bold;
-    }
-  }
-
- th {
-  background: linear-gradient(90deg, #43C7FF 0%, #44A9FF 64.99%, #4584FF 102.13%);
-  border-radius: 20px 20px 0px 0px;
-}
-
 .card1 {
   //position: absolute;
-  width: 315px;
+  width: auto;
   height: 132px;
+  h1 {
+    font-size: 25px;
+  }
 }
 
 .tg-hmp39{
